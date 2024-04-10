6867ba83d3a01ff2e4a41a0332a020de384469de
Kimia Arjmand <kimiya.arjmand8@gmail.com>
2024-04-11 00:33:20 +0330

    doc for college

--- src/main/java/ir/segroup/unipoll/ws/controller/CollegeController.java
+++ src/main/java/ir/segroup/unipoll/ws/controller/CollegeController.java
@@ -1,5 +1,10 @@
 package ir.segroup.unipoll.ws.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import ir.segroup.unipoll.shared.model.BaseApiResponse;
 import ir.segroup.unipoll.ws.model.request.CollegeRequest;
 import ir.segroup.unipoll.ws.service.CollegeService;
@@ -19,6 +24,25 @@ public class CollegeController {
     }
 
     @PostMapping
+    @Operation(summary = "Build a college in database")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "409",
+                    description = "College has already been built in database",
+                    content = {@Content(mediaType = "application/json"), @Content(mediaType = "application/xml")}
+            ),
+
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "System Default Exception (SDE) , or when database IO exception occurred",
+                    content = {@Content(mediaType = "application/json"), @Content(mediaType = "application/xml")}
+            ),
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "Create college successfully",
+                    content = {@Content(mediaType = "application/json"), @Content(mediaType = "application/xml")}
+            )
+    })
     public ResponseEntity<BaseApiResponse> createCollege(@RequestBody CollegeRequest collegeRequest){
         return collegeService.createCollege(collegeRequest);
 
