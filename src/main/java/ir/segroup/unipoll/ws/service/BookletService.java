4927688843a6b7750da62befd1737bfd10e8d286
Mary Hspr <maryam.hspr8@gmail.com>
2024-05-08 14:03:57 +0430

    edit upload booklet (get token)

--- src/main/java/ir/segroup/unipoll/ws/service/BookletService.java
+++ src/main/java/ir/segroup/unipoll/ws/service/BookletService.java
@@ -7,7 +7,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 public interface BookletService  {
     ResponseEntity<byte[]> downloadBooklet(String publicId);
-    ResponseEntity<BaseApiResponse> uploadBooklet(MultipartFile booklet, BookletRequest bookletRequest);
+    ResponseEntity<BaseApiResponse> uploadBooklet(MultipartFile booklet, BookletRequest bookletRequest, String token);
     ResponseEntity<BaseApiResponse> getTenTopBooklets(String token);
     ResponseEntity<BaseApiResponse> likeABooklet(String token,String bookletPublicId);
 }
